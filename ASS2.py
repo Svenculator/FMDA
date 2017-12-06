6f08bd8ad51e96d4029b10d759f2f8348e8f6dd1
Sven <savb94@gmail.com>
2017-12-06 12:17:41 +0100

    Whole new code, panda didn't work and other things had to be in it

--- ASS2.py
+++ ASS2.py
@@ -15,21 +15,54 @@ os.chdir('D:\\Dropbox\\Universiteit\\FMDA') # Set working directory
 print('Working directory is:', os.getcwd())
 
 #%%
-try: #Misschien ook with gebruiken op het laatste als ik tijd over heb en functie van maken
-    GHG_EU = open('UNFCCC_v20.csv', 'r') #open file
-    print(GHG_EU)
-except: 
-    print('Open failed')    #Build in fail safe
 
-head = GHG_EU.readline()
-
-print(head)
-
+def GHG(countrycode):
+    try:
+        with open('UNFCCC_v20.csv', 'r') as GHG_EU:
+            readGHG = csv.reader(GHG_EU, delimiter = '\t')
+            header=next(readGHG, None) #Skip header
+            countriesdata=dict()
+            sectorsdata=dict()
+            main_data= dict()
+            main_data={'countrycode': [], 
+                       'formatname' : [], 
+                       'polname': [], 
+                       'year' : [],
+                       'sectorname': [],
+                       'parentsectorcode': [],
+                       'sectorcode': [],
+                       'notation' : [],
+                       'unit': [],
+                       'emissions': [],
+                       'datasource' : [],
+                       'publicationdate' :[]}  
+            for row in readGHG:
+                countryc= str(row[0])
+                countriesdata[countrycode]= row[1]
+                sectorcode = str(row[7])
+                sectorsdata[sectorcode]=row[5], row[6]
+                
+                main_data['countrycode'].append(row[0])
+                main_data['polname'].append(row[3])
+                main_data['year'].append(row[4])
+                main_data['unit'].append(row[9])
+                main_data['emissions'].append(row[10])
+                main_data['sectorcode'].append(row[7])
+                main_data['parentsectorcode'].append(row[6])
+                
+                        
+            for x in range (len(main_data['countrycode'])):
+                if main_data['countrycode'][x] == countrycode:
+                    if main_data['sectorcode'][x] is not ==main_data['parentsector'][x]                    
+        
+    except:
+        print('Cannot find country')
+        
+#zoeken naar 0 om te weten of er geen data mist en uitleggen waarom de data mist. Bijv fishiries in een land waar geen water is.
+        
 #%%
-with open('UNFCCC_v20.csv', 'r') as GHG_EU:
-    readGHG = csv.reader(GHG_EU, delimiter = '\t')
-    header=next(readGHG, None) #Skip header
-    list_CC=[]
+
+ list_CC=[]
     list_CN=[]
     list_FN=[]
     list_PN=[]
@@ -42,38 +75,17 @@ with open('UNFCCC_v20.csv', 'r') as GHG_EU:
     list_E=[]
     list_DS=[]
     list_PD=[]
-    for row in readGHG:
-        CC= row[0]
-        CN= row[1]
-        FN= row[2]
-        PN= row[3]
-        Y= row[4]
-        SN= row[5]
-        PSC= row[6]
-        SC= row[7]
-        N= row[8]
-        U= row[9]
-        E= row[10]
-        DS= row[11]
-        PD= row[12]
-        
-        list_CC.append(CC)
-        list_CN.append(CN)
-        list_FN.append(FN)
-        list_PN.append(PN)
-        list_Y.append(Y)
-        list_SN.append(SN)
-        list_PSC.append(PSC)
-        list_SC.append(SC)
-        list_N.append(N)
-        list_U.append(U)
-        list_E.append(E)
-        list_DS.append(DS)
-        list_PD.append(PD)
-        
-#mytuple = tuple(mylist) #To make it a tuple
-      
-print(list_CC[0:100])        
+
+dict_CC = dict(zip(list_CC, list_CN))
+dict_sec= dict(zip(list_SC, list_SN))
+
+dict_CC['SI']
+dict_sec['1.A.2']
+
+print(header)
 
 
-  
+#%%
+print(head)
+
+def GHG(countrycode, year, pollutant, sectorcode):
