05050da8870ed5894761ffe97b480e26ed2a3974
Sven <savb94@gmail.com>
2017-12-06 14:26:13 +0100

    Now a different function file is created

---
+++ ASS2 main.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  6 14:17:41 2017
+
+@author: Sven
+This is where the magic happens
+"""
+#%%
+import os
+import ASS2 as fn
+
+
+#%% Set the working directory to what you want it to be
+
+os.chdir('D:\\Dropbox\\Universiteit\\FMDA') # Set working directory
+print('Working directory is:', os.getcwd())
+
+#%%
+#zoeken naar 0 om te weten of er geen data mist en uitleggen waarom de data mist. Bijv fishiries in een land waar geen water is.
+
+slovenia = fn.GHGcountry('SI')
