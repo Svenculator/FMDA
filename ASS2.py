cdc38bffa6e7c654cf7014c42fb971bf94d02556
Sven <savb94@gmail.com>
2017-12-06 12:20:52 +0100

    This one is the newest!

--- ASS2.py
+++ ASS2.py
@@ -16,76 +16,95 @@ print('Working directory is:', os.getcwd())
 
 #%%
 
-def GHG(countrycode):
-    try:
-        with open('UNFCCC_v20.csv', 'r') as GHG_EU:
-            readGHG = csv.reader(GHG_EU, delimiter = '\t')
-            header=next(readGHG, None) #Skip header
-            countriesdata=dict()
-            sectorsdata=dict()
-            main_data= dict()
-            main_data={'countrycode': [], 
-                       'formatname' : [], 
-                       'polname': [], 
-                       'year' : [],
-                       'sectorname': [],
-                       'parentsectorcode': [],
-                       'sectorcode': [],
-                       'notation' : [],
-                       'unit': [],
-                       'emissions': [],
-                       'datasource' : [],
-                       'publicationdate' :[]}  
-            for row in readGHG:
-                countryc= str(row[0])
-                countriesdata[countrycode]= row[1]
-                sectorcode = str(row[7])
-                sectorsdata[sectorcode]=row[5], row[6]
-                
-                main_data['countrycode'].append(row[0])
-                main_data['polname'].append(row[3])
-                main_data['year'].append(row[4])
-                main_data['unit'].append(row[9])
-                main_data['emissions'].append(row[10])
-                main_data['sectorcode'].append(row[7])
-                main_data['parentsectorcode'].append(row[6])
-                
-                        
-            for x in range (len(main_data['countrycode'])):
-                if main_data['countrycode'][x] == countrycode:
-                    if main_data['sectorcode'][x] is not ==main_data['parentsector'][x]                    
+def GHG(countrycode, year, pollutant, sectorcode): #With this function you can call any emmission of any country/sector/pollutant
+    with open('UNFCCC_v20.csv', 'r') as GHG_EU:
+        readGHG = csv.reader(GHG_EU, delimiter = '\t')
+        header=next(readGHG, None) #Skip header
+        countriesdata=dict()
+        sectorsdata=dict()
+        main_data= dict()
+        main_data={'countrycode': [], 
+                   'formatname' : [], 
+                   'polname': [], 
+                   'year' : [],
+                   'sectorname': [],
+                   'parentsectorcode': [],
+                   'sectorcode': [],
+                   'notation' : [],
+                   'unit': [],
+                   'emissions': [],
+                   'datasource' : [],
+                   'publicationdate' :[]}  
+        for row in readGHG:
+            countryc= str(row[0])
+            countriesdata[countryc]= row[1]
+            sector = str(row[7])
+            sectorsdata[sector]=row[5], row[6]
+            
+            main_data['countrycode'].append(row[0])
+            main_data['polname'].append(row[3])
+            main_data['year'].append(row[4])
+            main_data['unit'].append(row[9])
+            main_data['emissions'].append(row[10])
+            main_data['sectorcode'].append(row[7])
+            main_data['parentsectorcode'].append(row[6])
+            
+                    
+        for x in range (len(main_data['countrycode'])):
+            if main_data['countrycode'][x] == countrycode:
+                if main_data['year'][x] == year:
+                    if main_data['polname'][x]== pollutant:
+                        if main_data['sectorcode'][x] == sectorcode:
+                            print('In',countriesdata[countrycode],'the emission of',pollutant,'in',year,
+                                  'in sector',sectorsdata[sectorcode],'are', main_data['emissions'][x],main_data['unit'][x])
+                            
         
-    except:
-        print('Cannot find country')
+GHG('AT', '1992', 'CH4','1.A.1')
         
 #zoeken naar 0 om te weten of er geen data mist en uitleggen waarom de data mist. Bijv fishiries in een land waar geen water is.
         
-#%%
+#%% Nu alles van een heel land
 
- list_CC=[]
-    list_CN=[]
-    list_FN=[]
-    list_PN=[]
-    list_Y=[]
-    list_SN=[]
-    list_PSC=[]
-    list_SC=[]
-    list_N=[]
-    list_U=[]
-    list_E=[]
-    list_DS=[]
-    list_PD=[]
+#def GHG(countrycode, year, pollutant, sectorcode): #With this function you can call any emmission of any country/sector/pollutant
 
-dict_CC = dict(zip(list_CC, list_CN))
-dict_sec= dict(zip(list_SC, list_SN))
+with open('UNFCCC_v20.csv', 'r') as GHG_EU:
+    readGHG = csv.reader(GHG_EU, delimiter = '\t')
+    header=next(readGHG, None) #Skip header
+    countriesdata=dict()
+    sectorsdata=dict()
+    main_data= dict()
+    main_data={'countrycode': [], 
+               'formatname' : [], 
+               'polname': [], 
+               'year' : [],
+               'sectorname': [],
+               'parentsectorcode': [],
+               'sectorcode': [],
+               'notation' : [],
+               'unit': [],
+               'emissions': [],
+               'datasource' : [],
+               'publicationdate' :[]}  
+    for row in readGHG:
+        countryc= str(row[0])
+        countriesdata[countryc]= row[1]
+        sector = str(row[7])
+        sectorsdata[sector]=row[5], row[6]
+        
+        main_data['countrycode'].append(row[0])
+        main_data['polname'].append(row[3])
+        main_data['year'].append(row[4])
+        main_data['unit'].append(row[9])
+        main_data['emissions'].append(row[10])
+        main_data['sectorcode'].append(row[7])
+        main_data['parentsectorcode'].append(row[6])
+        
+                
+    for x in range (len(main_data['countrycode'])):
+        if main_data['countrycode'][x] == countrycode:
+            if main_data['year'][x] == year:
+                if main_data['polname'][x]== pollutant:
+                    if main_data['sectorcode'][x] == sectorcode:
+                        print('In',countriesdata[countrycode],'the emission of',pollutant,'in',year,
+                              'in sector',sectorsdata[sectorcode],'are', main_data['emissions'][x],main_data['unit'][x])
 
-dict_CC['SI']
-dict_sec['1.A.2']
-
-print(header)
-
-
-#%%
-print(head)
-
-def GHG(countrycode, year, pollutant, sectorcode):
