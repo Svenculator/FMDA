a777a2a0c51cb8e7294e13b49cbc824120a9a3a9
Sven <savb94@gmail.com>
2017-12-07 11:24:34 +0100

    Update

--- ASS2_function.py
+++ ASS2_function.py
@@ -8,7 +8,9 @@ Assignment 2 FMDA
 #%%
 import os
 import csv
-
+import re
+import matplotlib.pyplot as plot
+from math import log10
 #%% Set the working directory to what you want it to be
 
 os.chdir('D:\\Dropbox\\Universiteit\\FMDA') # Set working directory
@@ -114,11 +116,52 @@ def GHGcountry(countrycode):
     except:
         print('Only enter countrycodes which exist in the database as strings')
 
-
 #%%
-def filterzero(file): #function to filer out all the zeroes of the data
+def filterzero(file): #function to filter out all the zeroes of the data, so where no data is available
     list_nozero=[]
     for line in file:
         if line >0:
             list_nozero.append(line)
     return list_nozero
+
+#%% Benford formula
+
+def plot_benford(iterable):
+    #Plot leading digit distribution in a string iterable.
+
+    numbers = [float(n) for n in range(1, 10)]
+
+    # Plot the frequencies as predicted by the law.
+    benford = [log10(1 + 1 / d) for d in numbers]
+    plot.plot(numbers, benford, 'ro', label = "Predicted")
+
+    # Plot the actual digit frequencies.
+    data = list(digits(iterable))
+    plot.hist(data, range(1, 11), align = 'left', normed = True,
+              rwidth = 0.7, label = "Actual")
+
+    # Set plot parameters and show it in a window.
+    plot.title("Benford's Law")
+    plot.xlabel("Digit")
+    plot.ylabel("Frequency")
+
+    plot.xlim(0, 10)
+    plot.xticks(numbers)
+    plot.legend()
+
+    plot.show()
+
+def digits(iterable):
+    """Yield leading digits of number-like strings in an iterable.
+    """
+
+    numexp = re.compile(r'\d+(\.\d+)?([eE]\d+)?')
+    leading = set("123456789")
+
+    for item in iterable:
+        for match in numexp.finditer(str(item)):
+            for digit in match.group(0):
+                if digit in leading:
+                    yield int(digit)
+                    break
+
--- ASS2_main.py
+++ ASS2_main.py
@@ -23,60 +23,11 @@ SI = fn.GHGcountry('SI') #This will put all the data of Slovenia in a variable
 print(SI[:50]) #Looks like there are a lott of zeroes so a lott of missing data
 
 newSI= fn.filterzero(SI) #Filter out all non data 
+print(newSI[:50]) #Zeroes are gone! 
 
-#%% Benford formula
+newerSI=fn.beginzero(newSI)
 
-#Benford's law in Python.
-
-
-import re
-import matplotlib.pyplot as plot
-
-from math import log10
-
-def plot_benford(iterable):
-    #Plot leading digit distribution in a string iterable.
-
-    numbers = [float(n) for n in range(1, 10)]
-
-    # Plot the frequencies as predicted by the law.
-    benford = [log10(1 + 1 / d) for d in numbers]
-    plot.plot(numbers, benford, 'ro', label = "Predicted")
-
-    # Plot the actual digit frequencies.
-    data = list(digits(iterable))
-    plot.hist(data, range(1, 11), align = 'left', normed = True,
-              rwidth = 0.7, label = "Actual")
-
-    # Set plot parameters and show it in a window.
-    plot.title("Benford's Law")
-    plot.xlabel("Digit")
-    plot.ylabel("Frequency")
-
-    plot.xlim(0, 10)
-    plot.xticks(numbers)
-    plot.legend()
-
-    plot.show()
-
-def digits(iterable):
-    """Yield leading digits of number-like strings in an iterable.
-    """
-
-    numexp = re.compile(r'\d+(\.\d+)?([eE]\d+)?')
-    leading = set("123456789")
-
-    for item in iterable:
-        for match in numexp.finditer(str(item)):
-            for digit in match.group(0):
-                if digit in leading:
-                    yield int(digit)
-                    break
-                
-
-
-#if _name_ == "_main_":
-#    import fileinput
-#    import random
-#    my_randoms = random.sample(range(1000), 1000)
-#    plot_benford(my_randoms)
+#%%
+#sven=fn.digits(newSI)
+#print(sven)
+#fn.plot_benford(newSI)
